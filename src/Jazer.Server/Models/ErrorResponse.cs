66d104f83ddede626019393c163b34802a7c680e
James Wilson <tsunyoku@gmail.com>
2025-06-21 18:25:15 +0100

    Try a new error format

--- src/Jazer.Server/Models/ErrorResponse.cs
+++ src/Jazer.Server/Models/ErrorResponse.cs
@@ -7,5 +7,5 @@ namespace Jazer.Server.Models;
 public sealed class ErrorResponse(IEnumerable<IError> errors)
 {
     [JsonPropertyName("errors")]
-    public IEnumerable<IError> Errors { get; } = errors;
+    public string[] Errors => errors.SelectMany(x => x.Reasons).Select(x => x.Message).ToArray();
 }
