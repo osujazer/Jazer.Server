bb852485ef24dad23c1c5060622b6b140adea6d7
James Wilson <tsunyoku@gmail.com>
2025-06-22 19:16:34 +0100

    Add support to upload and delete avatar

--- src/Jazer.Server/Controllers/AvatarController.cs
+++ src/Jazer.Server/Controllers/AvatarController.cs
@@ -1,4 +1,6 @@
 using Asp.Versioning;
+using Jazer.Server.Extensions;
+using Jazer.Server.Models;
 using Jazer.Server.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -11,6 +13,11 @@ namespace Jazer.Server.Controllers;
 [ApiVersion("1.0")]
 public class AvatarController : ControllerBase
 {
+    private const int MaxFileSizeMb = 5;
+
+    private static readonly ErrorResponse InvalidPngError = new ErrorResponse("Image must be valid PNG file");
+    private static readonly ErrorResponse FileTooLargeError = new ErrorResponse($"File must not be larger than {MaxFileSizeMb}MB");
+
     [HttpGet("{userId:int}")]
     public async Task<FileContentHttpResult> GetUserAvatar(
         [FromRoute] int userId,
@@ -22,5 +29,40 @@ public class AvatarController : ControllerBase
         return TypedResults.File(
             userAvatar,
             contentType: "image/png");
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<Results<NoContent, UnprocessableEntity<ErrorResponse>>> UpdateUserAvatar(
+        IFormFile file,
+        [FromServices] IAvatarService avatarService,
+        CancellationToken cancellationToken)
+    {
+        if (!file.IsPng())
+            return TypedResults.UnprocessableEntity(InvalidPngError);
+        
+        if (file.Length > MaxFileSizeMb * 1024 * 1024)
+            return TypedResults.UnprocessableEntity(FileTooLargeError);
+
+        using var memoryStream = new MemoryStream();
+        await file.CopyToAsync(memoryStream, cancellationToken);
+        
+        await avatarService.SetAvatar(
+            User.GetUserId(), 
+            memoryStream.ToArray(),
+            cancellationToken);
+
+        return TypedResults.NoContent();
+    }
+
+    [HttpDelete]
+    [Authorize]
+    public async Task<NoContent> DeleteUserAvatar(
+        [FromServices] IAvatarService avatarService,
+        CancellationToken cancellationToken)
+    {
+        await avatarService.DeleteAvatar(User.GetUserId(), cancellationToken);
+        
+        return TypedResults.NoContent();
     }
 }
